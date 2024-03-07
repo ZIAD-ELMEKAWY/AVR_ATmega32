bd97325e23090144fbeef245fa640a41a49b8624
Ziad 7 <ziadelmekawy7@gmail.com>
2024-03-07 23:06:03 +0200

    Include Bit operation

---
+++ MCAL/DIO/DIO_interface.h
@@ -0,0 +1,62 @@
+﻿/*
+ * DIO_interface.h
+ * Layer   : MCAL
+ * SWC	   : GPIO 
+ * Author  : Ziad
+ * Created : 06/03/2024 05:36:44 م
+ */ 
+
+#ifndef DIO_INTERFACE_H_
+#define DIO_INTERFACE_H_
+
+/*********** Includes Section *******************/
+#include "Math.h"
+#include "Std_Types.h"
+/*********** Macro Function Declaration Section *******************/
+/**************** Macro Declaration Section ***********************/
+
+#define DIO_PIN_OUTPUT 1
+#define DIO_PIN_INPUT  0
+
+#define DIO_PORT_OUTPUT 0xFF
+#define DIO_PORT_INPUT  0x00
+
+#define DIO_PIN_HIGH 1
+#define DIO_PIN_LOW  0
+
+#define DIO_PORT_HIGH 1
+#define DIO_PORT_LOW  0
+
+#define DIO_PORTA 0
+#define DIO_PORTB 1
+#define DIO_PORTC 2
+#define DIO_PORTD 3
+
+#define DIO_PIN0 0
+#define DIO_PIN1 1
+#define DIO_PIN2 2
+#define DIO_PIN3 3
+#define DIO_PIN4 4
+#define DIO_PIN5 5
+#define DIO_PIN6 6
+#define DIO_PIN7 7
+/************** Data Types Declaration Section ********************/
+typedef enum
+{
+	DIO_NOK,
+	DIO_OK
+}DIO_ErrorStatus;
+
+/*************** Function Declaration Section *********************/
+// Pin
+DIO_ErrorStatus DIO_enum_SetPinDirection  (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 Copy_u8_Direction);
+DIO_ErrorStatus DIO_enum_SetPinVlaue      (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 Copy_u8_Value);
+DIO_ErrorStatus DIO_enum_GetPinValue      (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 *Copy_PtrData);
+DIO_ErrorStatus DIO_enum_TogglePinValue   (u8 Copy_u8PORT , u8 Copy_u8PiN);
+// Port
+DIO_ErrorStatus DIO_enum_SetPortDirection (u8 Copy_u8PORT ,  u8 Copy_u8_Direction);
+DIO_ErrorStatus DIO_enum_SetPortVlaue     (u8 Copy_u8PORT , u8 Copy_u8_Value);
+DIO_ErrorStatus DIO_enum_GetPortValue     (u8 Copy_u8PORT , u8 *Copy_PtrData );
+DIO_ErrorStatus DIO_enum_TogglePortValue  (u8 Copy_u8PORT );
+
+#endif /* DIO_INTERFACE_H_ */
---
+++ MCAL/DIO/DIO_program.c
@@ -0,0 +1,210 @@
+﻿/*
+ * DIO_program.c
+ * Layer   : MCAL
+ * SWC	   : GPIO
+ * Author  : Ziad
+ * Created : 06/03/2024 05:36:44 م
+ */
+
+/*********** Includes Section *******************/
+#include "DIO_interface.h" 
+#include "DIO_private.h"
+#include "DIO_config.h"
+
+/*********** Global variable Section *******************/
+
+/*********** Function Definition Section *******************/
+DIO_ErrorStatus DIO_enum_SetPinDirection  (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 Copy_u8_Direction)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if ((Copy_u8PORT <= DIO_PORTD ) && (Copy_u8PiN <= DIO_PIN7))
+	{
+		if (Copy_u8_Direction == DIO_PIN_OUTPUT)
+		{
+			switch (Copy_u8PORT)
+			{
+				case DIO_PORTA : SET_BIT(DDRA_Register , Copy_u8PiN); break;
+				case DIO_PORTB : SET_BIT(DDRB_Register , Copy_u8PiN); break;
+				case DIO_PORTC : SET_BIT(DDRC_Register , Copy_u8PiN); break;
+				case DIO_PORTD : SET_BIT(DDRD_Register , Copy_u8PiN); break;
+			}
+		}
+		else if (Copy_u8_Direction == DIO_PIN_INPUT)
+		{
+			switch (Copy_u8PORT)
+			{
+				case DIO_PORTA : CLR_BIT(DDRA_Register , Copy_u8PiN); break;
+				case DIO_PORTB : CLR_BIT(DDRB_Register , Copy_u8PiN); break;
+				case DIO_PORTC : CLR_BIT(DDRC_Register , Copy_u8PiN); break;
+				case DIO_PORTD : CLR_BIT(DDRD_Register , Copy_u8PiN); break;
+			}
+		}
+		else
+		{
+			LOC_enumState = DIO_NOK ;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState;
+}
+
+DIO_ErrorStatus DIO_enum_SetPinVlaue      (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 Copy_u8_Value)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if ((Copy_u8PORT <= DIO_PORTD) && (Copy_u8PiN <= DIO_PIN7))
+	{
+		if (Copy_u8_Value == DIO_PIN_HIGH)
+		{
+			switch (Copy_u8PORT)
+			{
+				case DIO_PORTA : SET_BIT(PORTA_Register , Copy_u8PiN); break;
+				case DIO_PORTB : SET_BIT(PORTB_Register , Copy_u8PiN); break;
+				case DIO_PORTC : SET_BIT(PORTC_Register , Copy_u8PiN); break;
+				case DIO_PORTD : SET_BIT(PORTD_Register , Copy_u8PiN); break;
+			}
+		}
+		else if (Copy_u8_Value == DIO_PIN_LOW)
+		{
+			switch (Copy_u8PORT)
+			{
+				case DIO_PORTA : CLR_BIT(PORTA_Register , Copy_u8PiN); break;
+				case DIO_PORTB : CLR_BIT(PORTB_Register , Copy_u8PiN); break;
+				case DIO_PORTC : CLR_BIT(PORTC_Register , Copy_u8PiN); break;
+				case DIO_PORTD : CLR_BIT(PORTD_Register , Copy_u8PiN); break;
+			}
+		}
+		else
+		{
+			LOC_enumState = DIO_NOK ;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState;
+}
+
+DIO_ErrorStatus DIO_enum_GetPinValue      (u8 Copy_u8PORT , u8 Copy_u8PiN , u8 *Copy_PtrData)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if ((Copy_u8PORT <= DIO_PORTD) && (Copy_u8PiN <= DIO_PIN7))
+	{
+		switch (Copy_u8PORT)
+		{
+			case DIO_PORTA : *Copy_PtrData = GET_BIT(PINA_Register , Copy_u8PiN); break;
+			case DIO_PORTB : *Copy_PtrData = GET_BIT(PINB_Register , Copy_u8PiN); break;
+			case DIO_PORTC : *Copy_PtrData = GET_BIT(PINC_Register , Copy_u8PiN); break;
+			case DIO_PORTD : *Copy_PtrData = GET_BIT(PIND_Register , Copy_u8PiN); break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+}
+
+DIO_ErrorStatus DIO_enum_TogglePinValue   (u8 Copy_u8PORT , u8 Copy_u8PiN)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if ((Copy_u8PORT <= DIO_PORTD ) && (Copy_u8PiN <= DIO_PIN7))
+	{
+		switch (DIO_PORTD)
+		{
+			case DIO_PORTA : TOGGLE_BIT(PORTA_Register , Copy_u8PiN); break;
+			case DIO_PORTB : TOGGLE_BIT(PORTB_Register , Copy_u8PiN); break;
+			case DIO_PORTC : TOGGLE_BIT(PORTC_Register , Copy_u8PiN); break;
+			case DIO_PORTD : TOGGLE_BIT(PORTD_Register , Copy_u8PiN); break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+}
+
+DIO_ErrorStatus DIO_enum_SetPortDirection (u8 Copy_u8PORT ,  u8 Copy_u8_Direction)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if (Copy_u8PORT <= DIO_PORTD)
+	{
+		switch (Copy_u8PORT)
+		{
+			case DIO_PORTA : DDRA_Register = Copy_u8_Direction ; break;
+			case DIO_PORTB : DDRB_Register = Copy_u8_Direction ; break;
+			case DIO_PORTC : DDRC_Register = Copy_u8_Direction ; break;
+			case DIO_PORTD : DDRD_Register = Copy_u8_Direction ; break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+}
+
+DIO_ErrorStatus DIO_enum_SetPortVlaue     (u8 Copy_u8PORT , u8 Copy_u8_Value)
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if (Copy_u8PORT <= DIO_PORTD)
+	{
+		switch (Copy_u8PORT)
+		{
+			case DIO_PORTA : PORTA_Register = Copy_u8_Value ; break;
+			case DIO_PORTB : PORTB_Register = Copy_u8_Value ; break;
+			case DIO_PORTC : PORTC_Register = Copy_u8_Value ; break;
+			case DIO_PORTD : PORTD_Register = Copy_u8_Value ; break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+	
+}
+
+DIO_ErrorStatus DIO_enum_GetPortValue     (u8 Copy_u8PORT , u8 *Copy_PtrData )
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if (Copy_u8PORT <= DIO_PORTD)
+	{
+		switch (Copy_u8PORT)
+		{
+			case DIO_PORTA : *Copy_PtrData = PINA_Register ; break;
+			case DIO_PORTB : *Copy_PtrData = PINB_Register ; break;
+			case DIO_PORTC : *Copy_PtrData = PINC_Register ; break;
+			case DIO_PORTD : *Copy_PtrData = PIND_Register ; break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+}
+
+DIO_ErrorStatus DIO_enum_TogglePortValue  (u8 Copy_u8PORT )
+{
+	DIO_ErrorStatus LOC_enumState = DIO_OK ;
+	if (Copy_u8PORT <= DIO_PORTD)
+	{
+		switch (Copy_u8PORT)
+		{
+			case DIO_PORTA : PORTA_Register = ~PORTA_Register ; break;
+			case DIO_PORTB : PORTB_Register = ~PORTB_Register ; break;
+			case DIO_PORTC : PORTC_Register = ~PORTC_Register ; break;
+			case DIO_PORTD : PORTD_Register = ~PORTD_Register ; break;
+		}
+	}
+	else
+	{
+		LOC_enumState = DIO_NOK ;
+	}
+	return LOC_enumState ;
+}
