a8033563d0bfc1181e7cf87962b7d58e430c3fd3
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2024-03-07 23:18:54 +0200

    LED Driver

---
+++ HAL/LED_config.h
@@ -0,0 +1,17 @@
+ /*
+ * LED_config.h
+ *	Layer  : HAL
+ * SWC	   : GPIO
+ * Author  : Ziad
+ * Created : 07/03/2024 19:16:37
+ */ 
+
+
+#ifndef LED_CONFIG_H_
+#define LED_CONFIG_H_
+
+
+
+
+
+#endif /* LED_CONFIG_H_ */
---
+++ HAL/Private.h
@@ -0,0 +1,16 @@
+/*
+ * Private.h
+ *
+ * Created: 07/03/2024 19:15:51
+ *  Author: hp
+ */ 
+
+
+#ifndef PRIVATE_H_
+#define PRIVATE_H_
+
+
+
+
+
+#endif /* PRIVATE_H_ */
