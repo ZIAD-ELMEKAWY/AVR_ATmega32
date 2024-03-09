61b61fdfe1ae24bb9f1a61614fb35f7dca3f1bae
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2024-03-09 02:51:34 +0200

    SSD_private

---
+++ HAL/SSD/SSD_private.h
@@ -0,0 +1,24 @@
+/*
+ * Layer   : MCAL
+ * SWC	   : GPIO
+ * Author  : Ziad
+ * Created: 07/03/2024 21:55:51
+ */ 
+
+
+#ifndef SSD_PRIVATE_H_
+#define SSD_PRIVATE_H_
+/*********** Includes Section *******************/
+#include "Math.h"
+#include "Std_Types.h"
+
+/*********** Macro Function Declaration Section *******************/
+/**************** Macro Declaration Section ***********************/
+#define SSD_COMM_CATHODE_NUMBER_ARR {0b00111111 , 0b00000110 , 0b01011011 , 0b01001111 , 0b01100110 , 0b01101101 , 0b01111101 , 0b00000111 , 0b01111111 , 0b01101111 }
+#define SSD_COMM_ANODE_NUMBER_ARR	{0xC0 , 0xF9 , 0xA4 , 0xB0 , 0x99 , 0x92 , 0x82 , 0xF8 , 0x80 , 0x90 }
+
+/************** Data Types Declaration Section ********************/
+
+/*************** Function Declaration Section *********************/
+
+#endif /* SSD_PRIVATE_H_ */
