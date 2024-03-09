c1cd92afec30286e3aa1314772c16aff70b8b430
ZIAD-ELMEKAWY <100296479+ZIAD-ELMEKAWY@users.noreply.github.com>
2024-03-09 03:01:32 +0200

    Switch

---
+++ HAL/SW/SW_config.h
@@ -0,0 +1,17 @@
+/*
+ * SW_config.h
+ *	Layer  : HAL
+ * SWC	   : GPIO
+ * Author  : Ziad
+ * Created: 09/03/2024 01:39:07
+ */ 
+
+
+#ifndef SW_CONFIG_H_
+#define SW_CONFIG_H_
+
+
+
+
+
+#endif /* SW_CONFIG_H_ */
